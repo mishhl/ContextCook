f4acf75f266a461577308c6975773ab1223a1beb
Aldyn Er <Aldyner4542@gmail.com>
2026-02-05 02:45:34 -0800

    Initial UI of Home page and Profile Page

---
+++ ContextCook/src/constants/data.jsx
@@ -0,0 +1,29 @@
+// FAKE DATA TO be replaced
+
+export const RecipesData = [
+    {
+        recipeName: "Kale Salad",
+        recipeDescription: "Vegan Friendly",
+        recipeTimeMinutes: 10
+    },
+    {
+        recipeName: "Fruit Parfait",
+        recipeDescription: "Breakfast",
+        recipeTimeMinutes: 10
+    },
+    {
+        recipeName: "Bologonese Spaghetti",
+        recipeDescription: "Lunch / Dinner",
+        recipeTimeMinutes: 15
+    },
+    {
+        recipeName: "Pepperoni Pizza",
+        recipeDescription: "Dinner",
+        recipeTimeMinutes: 60
+    },
+    {
+        recipeName: "Example Meal",
+        recipeDescription: "Title",
+        recipeTimeMinutes: 0
+    }
+]
